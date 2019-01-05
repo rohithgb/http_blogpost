f35cd19d8824e561598d779c7de457b73246f373
rohithgb <rohgb@outlook.com>
2019-01-06 00:57:55 +0530

    Fetching blog posts and onclick of post open fullpost

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,19 +3,55 @@ import React, { Component } from 'react';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
+
+import axios from 'axios';
+
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts:[],
+        seletedPostId: null
+    }
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response=>{
+                const posts = response.data.slice(0,4);
+                const updatedPosts = posts.map(post=>{
+                    return {
+                        ...post,
+                        author:'Rohith'
+                    }
+                })
+                this.setState({
+                    posts:updatedPosts,
+                })
+
+            })
+    }
+
+    selectecPost = (id) => {
+        this.setState({
+            seletedPostId:id,
+        })
+    }
+
     render () {
+        const posts = this.state.posts.map(post=>{
+            return <Post 
+                        key={post.id} 
+                        title={post.title} 
+                        author={post.author}
+                        clicked={()=>this.selectecPost(post.id)}/>;
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.seletedPostId}/>
                 </section>
                 <section>
                     <NewPost />
